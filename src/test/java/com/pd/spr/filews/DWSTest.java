ba050c85bfe79c2d6616366941763cc73b537fec
ralfspoeth <ralf.spoeth@profidata.com>
2022-03-23 16:59:16 +0100

    stable tests

--- src/test/java/com/pd/spr/filews/DWSTest.java
+++ src/test/java/com/pd/spr/filews/DWSTest.java
@@ -1,23 +1,31 @@
 package com.pd.spr.filews;
 
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
 
 public class DWSTest {
 
+    private static Path tmpDir = null;
+
+    @BeforeAll
+    static void initTmpDir() throws IOException {
+        tmpDir = Files.createTempDirectory(Path.of(System.getProperty("user.home")), "tmp");
+    }
+
     @Test
     public void testStatic() throws IOException, InterruptedException {
-        var t = DirectoryWatchService.startService(
-                DWSTest::logWatchEvent,
-                Path.of(new File(System.getProperty("user.home")).toURI()),
-                Path.of("."), Path.of("Desktop", "tmp"));
+        var t = DirectoryWatchService.startService(DWSTest::logWatchEvent, tmpDir);
         System.out.println("Started");
         long msecs = 60_000;
+        var f = tmpDir.resolve("demo.xml");
+        Files.createFile(f);
         Thread.sleep(msecs);
         System.out.printf("Ende nach %d msec%n", msecs);
     }
